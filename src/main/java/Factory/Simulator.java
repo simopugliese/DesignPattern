6af57a62ac14de1c36839ae171b3dad12aec23e3
Simone Pugliese <166115038+simopugliese@users.noreply.github.com>
2025-11-04 14:51:53 +0100

    Implemented Factory Pattern in Factory package

--- src/main/java/Factory/Simulator.java
+++ src/main/java/Factory/Simulator.java
@@ -1,16 +1,32 @@
 package Factory;
 
-import Factory.IngredientFactory.NaplesPizzaIngredientFactory;
-import Factory.IngredientFactory.PizzaIngredientFactory;
-import Factory.Pizza.CheesePizza;
 import Factory.Pizza.Pizza;
 import Factory.PizzaStore.NaplesPizzaStore;
 import Factory.PizzaStore.PizzaStore;
 
 public class Simulator {
     public static void main(String[] args) {
-        PizzaStore naplesPizzaStore = new NaplesPizzaStore();
-        PizzaIngredientFactory naplesIngredientFactory = new NaplesPizzaIngredientFactory();
-        Pizza pizza = new CheesePizza(naplesIngredientFactory);
+        // 1. Factory Method in azione:
+        // Creiamo la "fabbrica" (il punto vendita) per lo stile Napoletano.
+        PizzaStore naplesStore = new NaplesPizzaStore(); // Concrete Creator
+
+        // --- ORDINE 1: Cheese Pizza ---
+        System.out.println("--- Ordine per una Cheese Pizza Napoletana ---");
+
+        // orderPizza() è il Template Method che chiama internamente createPizza().
+        Pizza cheesePizza = naplesStore.orderPizza("cheese");
+
+        System.out.println("Ordine completato: " + cheesePizza.getName()); // Naples Style Cheese Pizza
+
+        System.out.println("\n-----------------------------------------------\n");
+
+        // --- ORDINE 2: Clam Pizza ---
+        System.out.println("--- Ordine per una Clam Pizza Napoletana ---");
+
+        // La logica di Abstract Factory (NaplesPizzaIngredientFactory) viene usata
+        // nella fase di prepare() per selezionare FreshClams, ThinCrustDough, ecc.
+        Pizza clamPizza = naplesStore.orderPizza("clam");
+
+        System.out.println("Ordine completato: " + clamPizza.getName()); // Naples Style Clam Pizza
     }
 }
