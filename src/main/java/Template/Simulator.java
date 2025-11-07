6232de603f19002a5b7f1f3fc8ffd5533b2a54cc
Simone Pugliese <166115038+simopugliese@users.noreply.github.com>
2025-11-07 09:39:21 +0100

    Implemented Template Pattern in Template package

--- src/main/java/Template/Simulator.java
+++ src/main/java/Template/Simulator.java
@@ -3,11 +3,10 @@ package Template;
 public class Simulator {
     public static void main(String[] args) {
         AbstractWithHook first = new MethodOne();
-        AbstractWithHook second = new MethodOne();
+        AbstractWithHook second = new MethodSecondWithHook();
+
         first.templateMethod();
-
         System.out.println("-------------------");
-
         second.templateMethod();
     }
 }
